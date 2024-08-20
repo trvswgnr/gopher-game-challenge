6b0fb68f13f9d75673928242ce4f2b990f913667
Travis Wagner <travis.wagner@umusic.com>
2024-08-20 17:18:14 -0400

    fix coin coint

--- coin.go
+++ coin.go
@@ -5,6 +5,11 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+const (
+	coinAttractionDistance = 5.0
+	coinLifespan           = 6.0
+)
+
 var (
 	coinSprite      = loadCoinSprite()
 	coins           []Coin
@@ -12,7 +17,8 @@ var (
 )
 
 type Coin struct {
-	x, y float64
+	x, y     float64
+	lifespan float64
 }
 
 func loadCoinSprite() *ebiten.Image {
@@ -21,7 +27,26 @@ func loadCoinSprite() *ebiten.Image {
 
 func (g *Game) dropCoin() {
 	if playerCoinCoint > 0 {
-		coins = append(coins, Coin{x: g.player.x, y: g.player.y})
+		// Calculate position in front of the player
+		coinX := g.player.x + g.player.dirX
+		coinY := g.player.y + g.player.dirY
+
+		coins = append(coins, Coin{x: coinX, y: coinY, lifespan: coinLifespan})
 		playerCoinCoint--
 	}
+}
+
+func (g *Game) updateCoins(deltaTime float64) {
+	// store active coins
+	activeCoinCount := 0
+	for i := range coins {
+		coins[i].lifespan -= deltaTime
+		if coins[i].lifespan > 0 {
+			// keep active coins
+			coins[activeCoinCount] = coins[i]
+			activeCoinCount++
+		}
+	}
+	// remove expired coins
+	coins = coins[:activeCoinCount]
 }
